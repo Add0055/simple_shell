b2f3f426663b36571a7859ec07353294031748f3
gizawsim <gizawsim@gmail.com>
2022-06-15 21:27:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,57 @@
 #include "main.h"
-
 /**
- * main - entry point for application
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success
+ * handle_ctrld - simple shell
+ * @vars: argument count
+ * @len: argument vectors
  */
-int main(int ac, char **av)
+void handle_ctrld(input_v *vars, int len)
 {
-	config build;
+	if (len == -1)
+	{
+		free(vars->buffer);
+		/*free(vars->array_inputs);*/
+		exit(0);
+	}
+}
+/**
+ * main - function that evaluates whether the command exists
+ * @ac: count
+ * @av: array
+ * @env: enviroment
+ * Return: message
+ */
+int main(int ac, char **av, char **env)
+{
+	input_v var = {0, NULL, NULL, NULL, 0};
+	size_t cant_buff = 0;
+	ssize_t len = 0;
+	char delimit[] = " \n\r\t";
 
 	(void)ac;
-	signal(SIGINT, sigintHandler);
-	configInit(&build);
-	build.shellName = av[0];
-	shell(&build);
-	return (0);
-}
-
-/**
- * configInit - initialize member values for config struct
- * @build: input build
- * Return: build with initialized members
- */
-config *configInit(config *build)
-{
-	build->env = generateLinkedList(environ);
-	build->envList = NULL;
-	build->args = NULL;
-	build->buffer = NULL;
-	build->path = _getenv("PATH", environ);
-	build->fullPath = NULL;
-	build->lineCounter = 0;
-	build->shellName = NULL;
-	build->errorStatus = 0;
-	return (build);
+	var.name_pro = av[0];
+	if (isatty(STDIN_FILENO))
+		write(1, "#cisfun$ ", 9);
+	while ((len = getline(&(var.buffer), &cant_buff, stdin)) != -1)
+	{
+		var.count++;
+		if (len > 1 && *var.buffer != '\n')
+		{
+			var.array_inputs = brokentoken(var.buffer, delimit);
+			if (var.array_inputs[0] != NULL)
+			{
+				var.exitstatus = check_for_comand(&var, env);
+				if (var.exitstatus == 0)
+					var.exitstatus =
+						comd_handling(&var, env);
+			}
+			else
+				free(var.array_inputs);
+		}
+		if (isatty(STDIN_FILENO))
+			write(1, "#cisfun$ ", 9);
+	}
+	if (isatty(STDIN_FILENO))
+		write(1, "\n", 1);
+	handle_ctrld(&var, len);
+	return (var.exitstatus);
 }
